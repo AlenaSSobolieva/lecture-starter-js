03c9e61258282fdd197f1ae3bcd018135f56ae47
Olena Sobolieva <soboleva13as@gmail.com>
2026-05-27 19:09:32 +0300

    BSA-310: Implement winner modal display after fight
    - Enhanced the showWinnerModal function to create and display a modal with the winner's image and name.
    - Updated the renderArena function to call showWinnerModal with the winner returned from the fight function, improving user feedback after combat.

--- src/javascript/components/arena.js
+++ src/javascript/components/arena.js
@@ -1,6 +1,7 @@
 import createElement from '../helpers/domHelper';
 import { createFighterImage } from './fighterPreview';
 import { fight } from './fight';
+import showWinnerModal from './modal/winner';
 
 function createFighter(fighter, position) {
     const imgElement = createFighterImage(fighter);
@@ -60,12 +61,13 @@ function createArena(selectedFighters) {
     return arena;
 }
 
-export default function renderArena(selectedFighters) {
+export default async function renderArena(selectedFighters) {
     const root = document.getElementById('root');
     const arena = createArena(selectedFighters);
 
     root.innerHTML = '';
     root.append(arena);
 
-    fight(...selectedFighters);
+    const winner = await fight(...selectedFighters);
+    showWinnerModal(winner);
 }
